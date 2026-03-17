bf147d3769ae3ab145de91c9e30252ae2e6302b7
Danilov Dmitriy <danilov0330@gmail.com>
2026-03-17 11:34:59 +0300

    refactor interface for user

--- main.go
+++ main.go
@@ -1,27 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 	"task_cli/cli"
 )
 
 func main() {
-	//for i, val := range os.Args {
-	//	fmt.Printf("Arguments #%d: %s\n", i, val)
-	//}
-
 	args := os.Args[1:]
-	//fmt.Println(args)
+
+	if len(args) == 0 {
+		fmt.Println("Use --help for more instructions")
+		return
+	}
 
 	switch args[0] {
+	case "--help":
+		printUsage()
 	case "add":
+		if len(args) < 2 {
+			fmt.Println("Error: Task description is required")
+			return
+		}
 		cli.AddTask(cli.Description(args[1]))
 	case "update":
-		id, _ := strconv.Atoi(args[1])
+		if len(args) < 3 {
+			fmt.Println("Error: ID and new description are required")
+			return
+		}
+		id, err := strconv.Atoi(args[1])
+		if err != nil {
+			fmt.Printf("Error: ID must be a number. Got: '%s'\n", args[1])
+			return
+		}
 		cli.UpdateTask(cli.Id(id), cli.Description(args[2]))
 	case "delete":
-		id, _ := strconv.Atoi(args[1])
+		if len(args) < 2 {
+			fmt.Println("Error: Task ID is required")
+			return
+		}
+		id, err := strconv.Atoi(args[1])
+		if err != nil {
+			fmt.Printf("Error: ID must be a number. Got: '%s'\n", args[1])
+			return
+		}
 		cli.DeleteTask(cli.Id(id))
 	case "list":
 		cli.GetTasks("") // Все задачи
@@ -32,10 +55,41 @@ func main() {
 	case "list-in-progress":
 		cli.GetTasks(cli.Statuses.InProgress) // Только в процессе
 	case "mark-in-progress":
-		id, _ := strconv.Atoi(args[1])
+		if len(args) < 2 {
+			fmt.Println("Error: Task ID is required")
+			return
+		}
+		id, err := strconv.Atoi(args[1])
+		if err != nil {
+			fmt.Printf("Error: ID must be a number. Got: '%s'\n", args[1])
+			return
+		}
 		cli.MarkInProgress(cli.Id(id))
 	case "mark-done":
-		id, _ := strconv.Atoi(args[1])
+		if len(args) < 2 {
+			fmt.Println("Error: Task ID is required")
+			return
+		}
+		id, err := strconv.Atoi(args[1])
+		if err != nil {
+			fmt.Printf("Error: ID must be a number. Got: '%s'\n", args[1])
+			return
+		}
 		cli.MarkDone(cli.Id(id))
+	default:
+		fmt.Printf("'%s': command not found. Use --help for more instructions", args[0])
 	}
+}
+
+func printUsage() {
+	fmt.Println("Task cli options:")
+	fmt.Println("\tadd <description>        		- Add new task")
+	fmt.Println("\tupdate <ID> <description>		- Update task's description")
+	fmt.Println("\tdelete <ID>                 		- Delete task")
+	fmt.Println("\tlist                        		- List all tasks")
+	fmt.Println("\tlist-done                   		- List all tasks that are done")
+	fmt.Println("\tlist-not-done               		- List all tasks that are not done")
+	fmt.Println("\tlist-in-progress            		- List all tasks that are in progress")
+	fmt.Println("\tmark-in-progress <ID>       		- Change task's status to 'in-progress'")
+	fmt.Println("\tmark-done <ID>              		- Change task's status to 'done'")
 }
